49cafbf1d15f0504f08460489520dd329abe7d01
Frank  D'Angelo <08fdange@gmail.com>
2020-02-14 19:15:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   reverse_array = []
   array = string.split
-  array.collect do |word|
+  array.each do |word|
      reverse_array << ("#{word.reverse}")
   end
   return reverse_array.join(" ")
